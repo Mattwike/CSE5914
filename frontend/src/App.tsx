6bb13d2910686382aa9515c09171c03cd8c35e06
Christopher Langenbahn <langenbahn.11@osu.edu>
2026-03-08 15:47:57 -0400

    added events page and event cards to populate it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./Screens/Login";
 import CreateAccount from "./Screens/CreateAccount";
 import Verify from './Screens/Verify';
 import Dashboard from "./Screens/Dashboard";
+import EventsPage from "./Screens/EventsPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/verify" element={<Verify />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/events" element={<EventsPage />} />
       </Routes>
     </BrowserRouter>
   );
