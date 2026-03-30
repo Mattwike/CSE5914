ecbf0c18ffb8b0e5a52bb689cb5dde93d919cf0b
Aaron-Dhillon <aarondhillon57@gmail.com>
2026-03-29 20:30:04 -0400

    fix email verification for serverless environment

--- backend/utils/sql_helper.py
+++ backend/utils/sql_helper.py
@@ -3,9 +3,10 @@ from sqlalchemy import create_engine, text
 
 class SQLHelper:
     def __init__(self):
-        pass
+        self._base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     def load_query(self, file_path: str) -> str:
-        with open(file_path, 'r') as file:
+        full_path = os.path.join(self._base_dir, file_path)
+        with open(full_path, 'r') as file:
             query = file.read()
         return text(query)
